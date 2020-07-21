b2b77bc39b72d586828d22a2ab6a627ea8e43371
Alonso Parra <alonsogparra@gmail.com>
2020-07-21 17:16:25 -0400

    App updated with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import EnterPhrase from './components/Game/EnterPhrase';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter basename="/">
+        <Switch>
+          <Route exact path="/" component={EnterPhrase} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
