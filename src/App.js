d78a1f48c66fa526f14bee5b5008f668ff6efa9e
Alonso Parra <alonsogparra@gmail.com>
2020-07-22 12:46:19 -0400

    Testing HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, HashRouter } from 'react-router-dom';
 import EnterPhrase from './components/Game/EnterPhrase';
 import Hangman from './components/Game/Hangman';
 
 function App() {
   return (
     <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" component={EnterPhrase} />
           <Route path="/game" component={Hangman} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
