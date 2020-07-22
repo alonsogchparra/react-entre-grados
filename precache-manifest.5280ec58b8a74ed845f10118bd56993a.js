b022a9672273c41c1ece3b75a453ab01e5981a4a
Alonso Parra <alonsogparra@gmail.com>
2020-07-21 21:58:11 -0400

    Updates

---
+++ precache-manifest.5280ec58b8a74ed845f10118bd56993a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998fdb78a7e86e4432266623ae8f5eee",
+    "url": "/react-entre-grados/index.html"
+  },
+  {
+    "revision": "5f0ba0b3d1b847ae43a0",
+    "url": "/react-entre-grados/static/css/2.cff28e81.chunk.css"
+  },
+  {
+    "revision": "b4e7639445de0e4d3905",
+    "url": "/react-entre-grados/static/css/main.1710b11a.chunk.css"
+  },
+  {
+    "revision": "5f0ba0b3d1b847ae43a0",
+    "url": "/react-entre-grados/static/js/2.0aae7d11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-entre-grados/static/js/2.0aae7d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e7639445de0e4d3905",
+    "url": "/react-entre-grados/static/js/main.86f7d8da.chunk.js"
+  },
+  {
+    "revision": "007a3e842bc1ab7375d0",
+    "url": "/react-entre-grados/static/js/runtime-main.7baba0b9.js"
+  },
+  {
+    "revision": "6ebfa8b9f5cb6acfd2b886831de06ba1",
+    "url": "/react-entre-grados/static/media/beber.6ebfa8b9.mp3"
+  },
+  {
+    "revision": "bc99a450f5b0a75c70abbd853cfa5d38",
+    "url": "/react-entre-grados/static/media/entregrados_logo.bc99a450.png"
+  },
+  {
+    "revision": "17e035b4b4087454a55b4682962399c2",
+    "url": "/react-entre-grados/static/media/higado_0.17e035b4.png"
+  },
+  {
+    "revision": "8f208556ff0e00aeccc52ede14042bc6",
+    "url": "/react-entre-grados/static/media/higado_1.8f208556.png"
+  },
+  {
+    "revision": "f3422a4897e81f7597f77a7d993d8044",
+    "url": "/react-entre-grados/static/media/higado_2.f3422a48.png"
+  },
+  {
+    "revision": "95e2b8acdbce75fe2a9284b211e0c24c",
+    "url": "/react-entre-grados/static/media/higado_3.95e2b8ac.png"
+  },
+  {
+    "revision": "14337e83a065523948c31385d5fe33d3",
+    "url": "/react-entre-grados/static/media/higado_4.14337e83.png"
+  },
+  {
+    "revision": "ebb14a06c02a0ffde799ec69d13b8243",
+    "url": "/react-entre-grados/static/media/higado_5.ebb14a06.png"
+  },
+  {
+    "revision": "74541a3ca896b999faf874b235969006",
+    "url": "/react-entre-grados/static/media/higado_6.74541a3c.png"
+  },
+  {
+    "revision": "5acc4c358abe356ab34cb46990213c87",
+    "url": "/react-entre-grados/static/media/victoria.5acc4c35.mp3"
+  }
+]);
